48bc30fde37f28499c45ac458adf970e983c3e38
Lokman Sarkar <lokmansarker13@gmail.com>
2025-06-28 17:56:05 +0600

    zod email validation controller fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,17 +16,27 @@ secure: true
 export const registerUser = async (req, res) => {
   const parse =  registerUserSchema.safeParse(req.body)
   if(!parse.success) {
-    return parse.error.errors[0].message
+    return res.status(400).json({message:parse.error.errors[0].message })
   }
 
   const {name, email, password} = parse.data
 
-  const existingUser = await  getUserByEmail(email)
+ try {
+  
+   const existingUser = await  getUserByEmail(email)
   if(existingUser) {
     return res.status(400).json({message: "Email already registerd"})
   }
 
   const user = await  createUser({name, email, password})
-  res.status(201).json({message: 'User Registerd Successfully', user: {id:user.id, email}})
+  if(!user) {
+    return res.status(500).json({message: "Failed to create user"})
+  }
+  return res.status(201).json({message: 'User Registerd Successfully', user: {id:user.id, email}})
+
+ } catch (error) {
+  console.log(error)
+  return res.status(500).json({message: "Something went wrong"})
+ }
 
 }
